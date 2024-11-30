e272556c5922311583618feacda661612fdc0bb3
ahpham123 <ahpham123@csu.fullerton.edu>
2024-11-30 01:51:14 -0800

    Implemented cleanup.py for step 1 of final project, does not handle line 9 correctly

---
+++ cleanup.py
@@ -0,0 +1,52 @@
+import re
+import os
+#TODO: STORE VARS IN LIST TO RECOGNIZE IN OPERATIONS, LINE 
+#bba = a1 * ( b2a + 2 * c) ;
+#IS SUPPOSED TO BE
+#bba = ( b2a + 2 * c) * a ;
+def clean_file(read, write):        #Read lines from input file(read) and store into list lines
+    try:
+        with open(read, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+    except UnicodeDecodeError:
+        print(f"Error: Unable to decode the file '{read}' with UTF-8 encoding.")
+    except FileNotFoundError:
+        print(f"Error: The file '{read}' was not found.")
+    except Exception as e:
+        print(f"A different error: {e} has occurred")
+
+    singleline = ''.join(lines)                 #Joins all lines into single string for re.sub formatting
+    singleline = re.sub(r'\(\*.*?\*\)', '', singleline, flags=re.DOTALL)        #Removes anything inbetween (* and *)
+
+    clean_lines = []
+    clean_lines = singleline.splitlines()       #Seperates giant list into respective lines and stores into new list
+
+    res = []
+    for line in clean_lines:
+        if line == "":
+            continue
+        line = re.sub(r'\s+', ' ', line).strip()        #r'\s+' is an expression for recognizing one or more whitespace chars (ie. space tab newline) and replaces them with singular ''
+        if line != "":                                                          #.sub deals with internal white space, .strip() deals with external white space (beginning or end)
+            if line == "end":
+                res. append(line)
+            else:
+                res.append(line + "\n")
+
+    res = [re.sub(r'\s*,\s*', ' , ', line) for line in res]             # Fix spaces around commas
+    for i, line in enumerate(res):
+        if i == 0:
+            continue
+        res[i] = re.sub(r'(?<!\s);', ' ;', line)                    # Add space before semicolons
+
+    try:                                    #Write correct lines into new file(write)
+        if os.path.exists(write):
+            with open(write, "w", encoding='utf-8') as file:            #If file already exists, wipe it before writing the editted version
+                pass
+        with open(write, "a+", encoding='utf-8') as file:               #Begin writing editted file, if file doesnt exist "a+" will make a newfile
+            for line in res:
+                file.writelines(line)
+    except Exception as e:
+        print(f"An error has occured: {e}")
+
+#EXAMPLE USAGE
+clean_file("final.txt", "empty.txt")
