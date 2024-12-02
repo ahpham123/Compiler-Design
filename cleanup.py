659493e74afc6ff56be83b64d800c16c4347e0b6
ahpham123 <ahpham123@csu.fullerton.edu>
2024-12-02 11:01:13 -0800

    cleanup.py now collects variables in text file

--- cleanup.py
+++ cleanup.py
@@ -1,10 +1,6 @@
 import re
 import os
-#TODO: STORE VARS IN LIST TO RECOGNIZE IN OPERATIONS, LINE 
-#bba = a1 * ( b2a + 2 * c) ;
-#IS SUPPOSED TO BE
-#bba = ( b2a + 2 * c) * a ;
-def clean_file(read, write):        #Read lines from input file(read) and store into list lines
+def clean_file(read, write):                                           #Read lines from input file(read) and store into list lines
     try:
         with open(read, 'r', encoding='utf-8') as file:
             lines = file.readlines()
@@ -15,30 +11,56 @@ def clean_file(read, write):        #Read lines from input file(read) and store 
     except Exception as e:
         print(f"A different error: {e} has occurred")
 
-    singleline = ''.join(lines)                 #Joins all lines into single string for re.sub formatting
-    singleline = re.sub(r'\(\*.*?\*\)', '', singleline, flags=re.DOTALL)        #Removes anything inbetween (* and *)
+    #Algorithim to get rid of comments
+    singleline = ''.join(lines)                                         #Joins all lines into single string for re.sub formatting
+    singleline = re.sub(r'\(\*.*?\*\)', '', singleline, flags=re.DOTALL)#Removes anything inbetween (* and *)
 
     clean_lines = []
-    clean_lines = singleline.splitlines()       #Seperates giant list into respective lines and stores into new list
+    clean_lines = singleline.splitlines()                               #Seperates giant list into respective lines and stores into new list
 
+    #Algorithim to get rid of spaces
     res = []
     for line in clean_lines:
         if line == "":
             continue
-        line = re.sub(r'\s+', ' ', line).strip()        #r'\s+' is an expression for recognizing one or more whitespace chars (ie. space tab newline) and replaces them with singular ''
-        if line != "":                                                          #.sub deals with internal white space, .strip() deals with external white space (beginning or end)
+        line = re.sub(r'\s+', ' ', line).strip()                        #r'\s+' is an expression for recognizing one or more whitespace chars (ie. space tab newline) and replaces them with singular ''
+        if line != "":                                                  #.sub deals with internal white space, .strip() deals with external white space (beginning or end)
             if line == "end":
                 res. append(line)
             else:
                 res.append(line + "\n")
 
+    #Algorithim to adjust spaces between commas
     res = [re.sub(r'\s*,\s*', ' , ', line) for line in res]             # Fix spaces around commas
     for i, line in enumerate(res):
         if i == 0:
             continue
-        res[i] = re.sub(r'(?<!\s);', ' ;', line)                    # Add space before semicolons
+        res[i] = re.sub(r'(?<!\s);', ' ;', line)                        # Add space before semicolons
 
-    try:                                    #Write correct lines into new file(write)
+    #Algorithim to collect variable names
+    varflag = False
+    vars = []
+    for line in res:
+        if line.lower() == "var\n":
+            varflag = True
+            continue
+        if varflag:
+            if line.lower() == "begin":
+                varflag = False
+            if varflag:
+                variables = re.findall(r'(\w+)(?=\s*,|\s*:)', line)     #Look for all raw strings with patterns of words (\w+) and look ahead (?=\s*,|\s*:) for commas or colons in line and make it into a list
+                for variable in variables:
+                    vars.append(variable)
+
+    #For testing, delete in final draft
+    print(vars)
+
+    #TODO: IMPLEMENT ALGORITHIM TO CORRECTLY USE VARS LIST OF VARIABLES
+    #CORRECT LINE IS bba = ( b2a + 2 * c) * a ;
+    #CURRENT LINE IS bba = a1 * ( b2a + 2 * c) ;
+
+
+    try:                                                                #Write correct lines into new file(write)
         if os.path.exists(write):
             with open(write, "w", encoding='utf-8') as file:            #If file already exists, wipe it before writing the editted version
                 pass
@@ -48,5 +70,5 @@ def clean_file(read, write):        #Read lines from input file(read) and store 
     except Exception as e:
         print(f"An error has occured: {e}")
 
-#EXAMPLE USAGE
+#EXAMPLE USAGE, reading from final.txt and writing into empty.txt
 clean_file("final.txt", "empty.txt")
